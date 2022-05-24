7071eba6ab814b9a5398e471804776bd49809633
Dexter Lowe <feldoh10@gmail.com>
2022-05-25 00:21:48 +0100

    Cookiecutter

---
+++ 1.3/Source/Magical_Diseases/Magical_Diseases.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Magical_Diseases
+{
+	public class Mod : Verse.Mod
+	{
+	public static Settings settings;
+		public Mod(ModContentPack content) : base(content)
+		{
+			Log.Message("Hello world from Magical Diseases");
+
+			// initialize settings
+			settings = GetSettings<Settings>();
+
+#if DEBUG
+			Harmony.DEBUG = true;
+#endif
+
+			Harmony harmony = new Harmony("Taggerung.rimworld.Magical_Diseases.main");	
+			harmony.PatchAll();
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			base.DoSettingsWindowContents(inRect);
+			settings.DoWindowContents(inRect);
+		}
+
+		public override string SettingsCategory()
+		{
+			return "Magical Diseases";
+		}
+	}
+}
